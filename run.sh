19b7a8574a72b7b94d9f3671854bc52e3dd73489
xvc <dev@xvc.com>
2026-03-03 15:26:01 +0800

    add tuzi

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -x
+
+if ! command -v bun 2>/dev/null; then
+  curl -fsSL https://bun.com/install | bash
+fi
+
+exec bun start $@
